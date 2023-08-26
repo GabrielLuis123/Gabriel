40ce2a07116f0c8dc012a0bb1d0316a61caccae4
GabrielLuis123 <141460456+GabrielLuis123@users.noreply.github.com>
2023-08-26 20:37:26 -0300

    Update atividade 04.py

--- atividade 04.py
+++ atividade 04.py
@@ -1,12 +1,54 @@
-# Leitura dos números e armazenamento na lista
-numeros = []
-for i in range(5):
-    numero = int(input(f"Digite o {i+1}º número inteiro: "))
-    numeros.append(numero)
+def listar_itens(lista):
+    print("Itens na lista de compras:")
+    for i, item in enumerate(lista, start=1):
+        print(f"{i}. {item}")
 
-# Soma dos valores da lista
-soma = 0
-for numero in numeros:
-    soma += numero
+def main():
+    lista_de_compras = []
 
-print(f"A soma dos números é: {soma}")
+    while True:
+        print("\nMenu:")
+        print("1. Adicionar item")
+        print("2. Remover item")
+        print("3. Editar item")
+        print("4. Listar itens")
+        print("5. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "1":
+            item = input("Digite o item a ser adicionado: ")
+            lista_de_compras.append(item)
+            print(f"{item} foi adicionado à lista.")
+
+        elif escolha == "2":
+            listar_itens(lista_de_compras)
+            indice = int(input("Digite o número do item a ser removido: ")) - 1
+            if 0 <= indice < len(lista_de_compras):
+                item_removido = lista_de_compras.pop(indice)
+                print(f"{item_removido} foi removido da lista.")
+            else:
+                print("Índice inválido.")
+
+        elif escolha == "3":
+            listar_itens(lista_de_compras)
+            indice = int(input("Digite o número do item a ser editado: ")) - 1
+            if 0 <= indice < len(lista_de_compras):
+                novo_item = input("Digite o novo valor: ")
+                lista_de_compras[indice] = novo_item
+                print("Item editado com sucesso.")
+            else:
+                print("Índice inválido.")
+
+        elif escolha == "4":
+            listar_itens(lista_de_compras)
+
+        elif escolha == "5":
+            print("Saindo do programa.")
+            break
+
+        else:
+            print("Opção inválida. Escolha novamente.")
+
+if _name_ == "_main_":
+    main()
